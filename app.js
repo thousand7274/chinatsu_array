2ceefb2ea6fffd284a02d620c3444b09f76ff2c5
thousand7274 <thousand7274@gmail.com>
2019-07-31 14:04:49 +0900

    array修正、追加
    二次元配列を見やすくして、三次元配列を追加しました。

--- app.js
+++ app.js
@@ -4,14 +4,16 @@
 // pink ピンク
 // red　赤
 // 連想配列
-// array2 = [['apple', 'orange','pink','red'],['りんご','みかん','ピンク','赤']];
+// array2 = [
+//   ['apple', 'orange','pink','red'],
+//   ['りんご','みかん','ピンク','赤']
+// ];
 // console.log(array2[1][1]);
 
 let fruits = {
   apple: 'りんご',
   orange: 'みかん'
 };
-
 let colors = {
   pink: 'ピンク',
   red: '赤'
@@ -19,3 +21,13 @@ let colors = {
  let fruitsColors = [fruits,colors];
 
  console.log(fruitsColors[0].orange);
+
+//  三次元配列
+let color =[
+  {
+  pink: 'ピンク',
+  red: '赤',
+  blue:['shoes','shirts','pants']
+}];
+console.log(color[0].blue[1]);
+
